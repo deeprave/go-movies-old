fa29b7b124d1652bf075cd93674ff69ede84f2d2
David Nugent <david.nugent@pexa.com.au>
2022-11-22 14:10:22 +1100

    simplify find* tests

--- api/app/file_test.go
+++ api/app/file_test.go
@@ -7,30 +7,47 @@ import (
 	"testing"
 )
 
-func TestFindPath(t *testing.T) {
-	home := "/home/testing"
+var testData = []byte("test:\n\t- first item\n\t- second item\n")
+var testHome = "/home/testing"
+var testFileName = "config.yml"
 
-	fs := afero.NewMemMapFs()
-
+func setUpHome(home string, fs afero.Fs, configFileName string) *pathlib.Path {
 	// make a "home" path
 	homePath := pathlib.NewPathAfero(home, fs)
-	err := homePath.MkdirAll()
-	test.ShouldBeNoError(t, err, "%v: %v", homePath, err)
-	ok, err := homePath.IsDir()
-	test.ShouldBeTrue(t, ok, "%v: %v", homePath, err)
+	_ = homePath.MkdirAll()
+	_, _ = homePath.IsDir()
 
 	// create a file in the home path with some data
-	configFile := pathlib.NewPathAfero("config.yml", fs)
+	configFile := pathlib.NewPathAfero(configFileName, fs)
 	configPath := homePath.JoinPath(configFile)
-	data := []byte("test:\n\t- first item\n\t- second item\n")
-	err = configPath.WriteFile(data)
-	test.ShouldBeNoError(t, err, "%v: %v", configFile, err)
+	_ = configPath.WriteFile(testData)
+	return homePath
+}
+
+func TestFindPath(t *testing.T) {
+
+	fs := afero.NewMemMapFs()
+	homePath := setUpHome(testHome, fs, testFileName)
+	configFile := pathlib.NewPathAfero(testFileName, fs)
 
 	// how try to locate it
 	foundPath, err := FindPath(configFile, ".", homePath)
 	test.ShouldBeNoError(t, err, "%v", err)
 	size, err := foundPath.Size()
 	test.ShouldBeNoError(t, err, "%v (size): %v", configFile, err)
-	test.ShouldBeEqual(t, len(data), int(size))
-	t.Logf("successfully found: %v, %d bytes", foundPath, size)
+	test.ShouldBeEqual(t, len(testData), int(size))
+	t.Logf("successfully found (path): %v, %d bytes", foundPath, size)
+}
+
+func TestFindFile(t *testing.T) {
+
+	fs := afero.NewMemMapFs()
+	homePath := setUpHome(testHome, fs, testFileName)
+
+	// how try to locate it
+	foundPath, err := FindFile(testFileName, fs, ".", homePath)
+	test.ShouldBeNoError(t, err, "%v", err)
+	if err == nil {
+		t.Logf("successfully found (file): %v", foundPath)
+	}
 }
